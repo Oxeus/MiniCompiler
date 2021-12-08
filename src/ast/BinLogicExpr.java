637ec43fa6bf0452c0bc1a3909f535185a856113
Adam La Fleur <13008923+Oxeus@users.noreply.github.com>
2021-12-07 17:47:30 -0800

    Initial Commit

---
+++ src/ast/BinLogicExpr.java
@@ -0,0 +1,31 @@
+package ast;
+import compiler.Failure;
+import compiler.Position;
+
+/** Abstract syntax for binary logical expressions.
+ */
+public abstract class BinLogicExpr extends BinExpr {
+
+    /** Default constructor.
+     */
+    public BinLogicExpr(Position pos, Expr left, Expr right) {
+        super(pos, left, right);
+    }
+
+    /** Run type checking analysis on this expression.  The typing
+     *  parameter provides access to the type analysis phase (in
+     *  particular, to the associated error handler).
+     */
+    public Type analyze(TypeAnalysis typing) {  // LAnd, LOr
+        left.require(typing, Type.BOOLEAN);
+        right.require(typing, Type.BOOLEAN);
+        return type = Type.BOOLEAN;
+    }
+
+    public VarSet analyze(InitAnalysis init, VarSet initialized) {
+	VarSet temp = left.analyze(init, initialized);
+	right.analyze(init, temp);
+       	return temp;
+    }
+}
+
